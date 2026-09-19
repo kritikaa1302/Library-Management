405ecac093826981d01a4da4ebb58017c4567804
kritikaa1302 <kritikagrover1302@gmail.com>
2026-09-19 19:27:18 +0530

    Remove unnecessary blank line in announcementRoutes.js

--- server/routes/announcementRoutes.js
+++ server/routes/announcementRoutes.js
@@ -2,7 +2,6 @@ import express from 'express';
 import { getAnnouncements, createAnnouncement, deleteAnnouncement } from '../controllers/announcementController.js';
 import { authenticate } from '../middleware/auth.js';
 import { isAdmin } from '../middleware/admin.js';
-
 const router = express.Router();
 
 router.get('/', getAnnouncements);
