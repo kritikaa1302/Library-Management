3560e6fb9f72f2c6b849ba56425b35a15a5a4263
kritikaa1302 <kritikagrover1302@gmail.com>
2026-09-19 19:27:38 +0530

    Update authRoutes.js

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import { register, login, getMe } from '../controllers/authController.js';
 import { authenticate } from '../middleware/auth.js';
-
 const router = express.Router();
 
 router.post('/register', register);
