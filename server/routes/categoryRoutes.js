12ea77d95dface0ebe3cced1a4aaeaf24842c8b2
kritikaa1302 <kritikagrover1302@gmail.com>
2026-09-19 19:26:49 +0530

    Update categoryRoutes.js

--- server/routes/categoryRoutes.js
+++ server/routes/categoryRoutes.js
@@ -2,7 +2,6 @@ import express from 'express';
 import { getCategories, createCategory, deleteCategory } from '../controllers/categoryController.js';
 import { authenticate } from '../middleware/auth.js';
 import { isAdmin } from '../middleware/admin.js';
-
 const router = express.Router();
 
 router.get('/', getCategories);
