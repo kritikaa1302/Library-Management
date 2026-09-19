28e5cb9037733975af16af514c76aa0fb0569865
kritikaa1302 <kritikagrover1302@gmail.com>
2026-09-19 19:27:27 +0530

    Update announcementRoutes.js

--- server/routes/announcementRoutes.js
+++ server/routes/announcementRoutes.js
@@ -3,9 +3,7 @@ import { getAnnouncements, createAnnouncement, deleteAnnouncement } from '../con
 import { authenticate } from '../middleware/auth.js';
 import { isAdmin } from '../middleware/admin.js';
 const router = express.Router();
-
 router.get('/', getAnnouncements);
 router.post('/', authenticate, isAdmin, createAnnouncement);
 router.delete('/:id', authenticate, isAdmin, deleteAnnouncement);
-
 export default router;
